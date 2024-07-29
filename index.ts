40b3fbd12f40d6f7b42da85bdf45440d976cd5c8
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-07-29 15:42:29 +0530

    inital db connection and error handling

--- index.ts
+++ index.ts
@@ -1,13 +1,54 @@
+import express from 'express';
+import env from 'dotenv';
+import cors from 'cors';
 import mongoose from 'mongoose';
+import { ConfigurationError, SystemError, ErrorHandlerMiddleware } from './src/middleware/error-middleware.ts';
 
-// Connect to MongoDB
-mongoose
-	.connect('mongodb://localhost:27017/hotel-management')
+env.config();
+const port = process.env.PORT || 3000;
+const environment = process.env.ENVIRONMENT || 'local';
+const service = process.env.SERVICE || 'local';
+const MONGO_URI = process.env.MONGO_URI;
+if (!MONGO_URI || MONGO_URI == undefined) {
+	throw new ConfigurationError('MONGO_URI is not set in the envionment');
+}
+const app = express().use(cors()).use(express.json());
 
-	.then(() => {
-		console.log('Connected to MongoDB');
-		// Your code here
-	})
-	.catch((error) => {
-		console.error('Failed to connect to MongoDB', error);
+async function connectToDB(mongoUri: string) {
+	try {
+		await mongoose.connect(mongoUri, {
+			dbName: 'hirestud',
+		});
+		console.log('database connected successfully');
+	} catch (err) {
+		console.log('error in connecting to mongoDB', err);
+	}
+}
+connectToDB(MONGO_URI);
+
+app.get('/home', (req, res) => {
+	res.send({
+		message: 'Service running',
+		dbConnected: mongoose.connection.readyState === 1,
+		environment,
 	});
+});
+app.get('/', (req, res) => res.send('Express on Vercel'));
+
+app.use((err: SystemError, req: express.Request, res: express.Response, next: express.NextFunction) => {
+	res.status(500).send({
+		error: err.message,
+		stack: err.stack,
+	});
+});
+
+app.use('*', (req, res) => {
+	res.status(404).send({
+		error: 'AUTH404: Could not find the page requested by you',
+	});
+});
+app.use(ErrorHandlerMiddleware);
+app.listen(port, () => {
+	console.log('App listening on port service is ', port, service);
+});
+// https://www.youtube.com/watch?v=T5t46vuW8fo&ab_channel=CodeBucks
