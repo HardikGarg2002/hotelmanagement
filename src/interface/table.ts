d758afac2776d007c0b9c0121d1520b5d63070ac
hardikgarg2002 <hardikgarg3085@gmail.com>
2024-08-14 14:10:01 +0530

    table in restaurant with floor specified to make reservation

---
+++ src/interface/table.ts
@@ -0,0 +1,15 @@
+export interface ITable {
+	_id?: string;
+	restaurant_slug: string;
+	slug: string;
+	capacity: number;
+	status: TableStatus;
+	floor: string; // floor
+}
+// table for 2, table for 4, table for 6, table for 8, table for 10
+
+export enum TableStatus {
+	AVAILABLE = 'AVAILABLE',
+	RESERVED = 'RESERVED',
+	OCCUPIED = 'OCCUPIED',
+}
